bb1d0be98182994cee7610ef2edf2acd8fd0c669
Paul <f1rstmarious@yandex.ru>
2023-07-18 15:23:37 +0300

    homework_completed

---
+++ task3.py
@@ -0,0 +1,12 @@
+def is_prime(n):
+    for i in range(2, int(n ** (1 / 2) + 1)):
+        if n % i == 0:
+            return "Число составное"
+    return "Число простое"
+
+
+number = int(input("Введите целое число от 1 до 100000: "))
+if 0 < number <= 100000:
+    print(is_prime(number))
+else:
+    print("Введите число в диапазоне от 1 до 100000")
